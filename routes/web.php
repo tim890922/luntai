f2ba1d63eea62d2f5f78c33be1408d0708611f07
tim890922 <vicki199444@gmail.com>
2022-08-07 15:20:53 +0800

    123

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,40 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\ProductController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/',
+    function () {
+        return view('welcome');
+    }
+);
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/product', [ProductController::class,'index']);
+// Route::get('/home',
+//     function () {
+//         return view('home',['name'=>'tim']);
+//     }
+// );
+// Route::get('/home/{name}',
+//     function ($name) {
+//         return view('home',['name'=>$name]);
+//     }
+// );
+
+Route::prefix('/home')->group(function(){
+    Route::get('/', function () {
+        return view('home');
+    });
+    Route::get('/{name}', function ($name) {
+        return view('home',['name'=>$name]);
+    });
+    Route::get('/', function () {
+        return view('home');
+    });
+
+
 });
+
+Route::redirect('/here', '/home');
+
+
