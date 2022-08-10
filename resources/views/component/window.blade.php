f849c447a7fec033ee71606df54a1bc8ffc89a96
tim890922 <vicki199444@gmail.com>
2022-08-10 16:37:13 +0800

    windows的原件小修

--- resources/views/component/window.blade.php
+++ resources/views/component/window.blade.php
@@ -2,22 +2,22 @@
     style="">
     <div class="w-1/2 p-4 mx-auto mt-20 bg-white rounded-md shadow-md opacity-100">
         <h2 class="mb-10 text-3xl font-bold text-center text-purple-500">
-            登入您的帳號
+            標題區
         </h2>
         <p class="mb-2">
-            電子信箱
+            lable
         </p>
         <input type="email" class="w-full p-2 border-2 border-gray-400 rounded-md focus:ring-2" />
         <p class="mt-4 mb-2">
-            密碼
+            lable
         </p>
         <input type="password" class="w-full p-2 border-2 border-gray-400 rounded-md focus:ring-2" />
         <a href="javascript:void(0)" class="block mt-2 text-blue-400">
-            忘記密碼
+            功能鍵
         </a>
         <button
             class="w-full py-3 mt-4 text-lg tracking-wide text-white duration-200 bg-purple-500 rounded-lg hover:bg-purple-600 active:bg-purple-900 active:ring-2">
-            登入
+            功能鍵
         </button>
     </div>
 </div>
